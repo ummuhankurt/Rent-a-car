c0ab1ad232bf56e8b0b4dc304ce280291e7195ee
ummuhan kurt <ummuhankurt.23@outlook.com>
2022-07-05 16:41:37 +0300

    Add project files.

---
+++ ConcoleUI/Program.cs
@@ -0,0 +1,45 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+
+namespace ConcoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+            Console.WriteLine("Bütün ürünler : ");
+            foreach (var product in productManager.GetAll())
+            {
+                Console.WriteLine(product.Description);
+            }
+            Console.WriteLine("Ürün eklndikten sonra ürünleri listeleme");
+            Car car = new Car { Id = 6, BrandId = 8, ColorId = 9, ModelYear = new DateTime(2014), DailyPrice = 178000, Description = "Mini Cooper" };
+            productManager.Add(car);
+            foreach (var product in productManager.GetAll())
+            {
+                Console.WriteLine(product.Description);
+            }
+            Console.WriteLine("id si 6 olan ürünün fiyatini 200000 olacak şekilde update ettik");
+            var carToUpdate = productManager.GetById(6);
+            carToUpdate.DailyPrice = 200000;
+            productManager.Update(carToUpdate);
+            foreach (var product in productManager.GetAll())
+            {
+                Console.WriteLine(product.Description + " " + product.DailyPrice);
+            }
+            Console.WriteLine("id si 6 olan productı getirme");
+            Console.WriteLine(productManager.GetById(6).Description);
+            Console.WriteLine("id si 6 olan ürünü sileim");
+            productManager.Delete(productManager.GetById(6));
+            foreach (var product in productManager.GetAll())
+            {
+                Console.WriteLine(product.Description);
+            }
+
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,58 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Car> _cars; // Bu class için global deiğşken.
+        public InMemoryProductDal()
+        {
+            // Oracle'dan veya Sql'den veya Postgres veya MongoDb'den gleiyormuş gibi...
+            _cars = new List<Car>
+            {
+                new Car{ Id = 1, BrandId = 5, ColorId = 3, DailyPrice = 350000, ModelYear = new DateTime(2015) , Description = "Audi A5"},
+                new Car{ Id = 2, BrandId = 2, ColorId = 5, DailyPrice = 100000, ModelYear = new DateTime(2012) , Description = "Opel Corse"},
+                new Car{ Id = 3, BrandId = 1, ColorId = 3, DailyPrice = 150000, ModelYear = new DateTime(2010) , Description = "Fiat Fiorino"},
+                new Car{ Id = 4, BrandId = 7, ColorId = 6, DailyPrice = 400000, ModelYear = new DateTime(2018) , Description = "Renault Megane"},
+                new Car{ Id = 5, BrandId = 2, ColorId = 2, DailyPrice = 350000, ModelYear = new DateTime(2015) , Description = "Opel Astra"}
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete;
+            carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public Car GetById(int id)
+        {
+            return _cars.SingleOrDefault(c => c.Id == id);
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate;
+            carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+    }
+}
